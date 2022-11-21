d74ebc4d8b2cccc74e674b1719c4e629376c77a6
Nightfall <intern.marcel@titipku.com>
2022-11-21 21:13:36 +0700

    add navbar

--- src/page/Home.js
+++ src/page/Home.js
@@ -8,12 +8,14 @@ import Sd from './Sd'
 // import Form from '../component/Form'
 import Footer from '../component/Footer'
 // import CarouselNews from '../component/Carousel'
+import Napbar from '../component/Napbar'
 
 
 const Home = () => {
   return (
     <div>
      <Navbar/>
+     <Napbar/>
      <Tk/>
      <Sd/>
      <Product/>

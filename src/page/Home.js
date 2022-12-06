ca756e012f045c198ff3057c5e6c82a2c852ef5b
Nightfall <marcelantonio246@gmail.com>
2022-12-07 00:03:44 +0700

    add organisasi

--- src/page/Home.js
+++ src/page/Home.js
@@ -63,6 +63,44 @@ const Home = () => {
               </div>
           </div>
           </section>
+
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="text-center mb-20">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Struktur Organisasi</h1>
+                <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
+                <div class="flex mt-6 justify-center">
+                    <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+                </div>
+                </div>
+                <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                    </div>
+                
+                
+                </div>
+                <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+            </div>
+            </section>
+
+
+
       
       </div>
      <Footer/>

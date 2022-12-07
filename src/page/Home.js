f0b66938f1065ace8e4d6b9f3b16b21961725f65
Nightfall <marcelantonio246@gmail.com>
2022-12-07 18:34:44 +0700

    add nama jabatan

--- src/page/Home.js
+++ src/page/Home.js
@@ -35,34 +35,28 @@ const Home = () => {
              
           </div>
           </section>
-      <News/>
+        <News/>
         <div id="sejarah" className="mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
-        <div className="w-full lg:w-6/12 ">
+            <div className="w-full lg:w-6/12 ">
                   <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
-              </div>
-              <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Sejarah</h1>
-                  <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
-              </div>
+            </div>
+            <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">TK CERDAS BANGSA</h1>
+                <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            </div>
              
-          </div>
+        </div>
+        <div id="sejarah" className="mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
+            <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">SD CERDAS BANGSA</h1>
+                <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            </div>
+            <div className="w-full lg:w-6/12 ">
+                  <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
+            </div>
+        </div>
           
-          <section className="pt-24 pb-36 bg-gray-800 overflow-hidden mt-5">
-          <div className="container px-4 mx-auto">
-              <h2 className="mb-4 text-6xl md:text-5xl text-center font-bold font-heading font-heading tracking-px-n leading-tight text-white">Struktur Organisasi</h2>
-              <p className="mb-24 font-medium text-white text-center leading-relaxed md:max-w-lg mx-auto">Lorem ipsum dolor sit amet, to the consectr adipiscing elit. Volutpat tempor to the condimentum vitae vel purus.</p>
-              <div className="flex flex-wrap mb-10">
-          <div className="bg-white h-44 w-44 rounded-full ml-7"></div>
-          <div className="bg-white h-44 w-44 rounded-full ml-7"></div>
-          <div className="bg-white h-44 w-44 rounded-full ml-7"></div>
-              </div>
-              <div className="flex flex-wrap mb-10">
-          <div className="bg-white h-44 w-44 rounded-full ml-7"></div>
-          <div className="bg-white h-44 w-44 rounded-full ml-7"></div>
-          <div className="bg-white h-44 w-44 rounded-full ml-7"></div>
-              </div>
-          </div>
-          </section>
+          
 
           <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
@@ -76,26 +70,37 @@ const Home = () => {
                 <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Nama</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Nama</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Nama</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Nama</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Nama</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Nama</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
                     </div>
                 
                 
                 </div>
-                <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
             </div>
             </section>
 

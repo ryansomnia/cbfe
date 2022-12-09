019c24dc0768e3ed09e90b46f554cfc9c7958d2e
70001511952020 <heriyanto.sitorus@brilife.co.id>
2022-12-09 20:50:23 +0700

    ppdb tk

--- src/page/PPDB.js
+++ src/page/PPDB.js
@@ -79,22 +79,7 @@ export default function PPDB() {
     </div>
     </div>
   <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full md:w-2/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-        Nama Lengkap
-      </label>
-      <input 
-      onChange={handleOnChange}
-      name = "namaLengkap"
-      className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
-
-    </div>
-    <div className="w-full md:w-2/3 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-        Nama Panggilan
-      </label>
-      <input name='NamaPanggilan' onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
-    </div>
+    
     <div className="w-full md:w-1/3 px-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
         Jenis Kelamin
@@ -103,6 +88,19 @@ export default function PPDB() {
             <option value="lakiLaki">Laki-Laki</option>
             <option value="perempuan">Perempuan</option>
           </select>
+    </div>
+    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+        Tanggal Lahir
+      </label>
+      <input name = "tanggalLahir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="date" placeholder="Albuquerque"/>
+    </div>
+    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+        Tempat Lahir
+      </label>
+      <input  name = "tempatLahir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
+
     </div>
   </div>
   
@@ -138,57 +136,8 @@ export default function PPDB() {
           </select>
     </div>
     </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Gereja
-      </label>
-      <input 
-      name = "gereja"
-      onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Kewarganegaraan
-      </label>
-      <input 
-       name = "kewarganegaraan"
-      onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-
-    </div>
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Jumlah Saudara
-      </label>
-      <input
-      name = "jumlahSaudara"
-      onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="number" />
-
-    </div>
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Anak ke -
-      </label>
-      <input 
-      name = "anakKe"
-      onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="number" />
-
-    </div>
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Golongan Darah
-      </label>
-      <select name = "golonganDarah" onChange={handleOnChange}className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' >
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="AB">AB</option>
-            <option value="O">O</option>
-          </select>
-    </div>
-    </div>
+   
+  
     <div className="flex flex-wrap -mx-3 mb-2">
     <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
@@ -250,199 +199,7 @@ export default function PPDB() {
     </div>
     
     </div>
-<h3 className='block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2 mt-7'>Identitas Ayah / Wali</h3>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Nama Ayah Kandung / Wali
-      </label>
-      <input name = "a_namaAyah" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-  
-    </div>
 
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Pendidikan Terakhir 
-      </label>
-      
-      <input name = "a_pendidikanTerakhir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Pekerjaan
-      </label>
-      <input name = "a_pekerjaan" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-    </div>
-    </div>
-
-    <div className="flex flex-wrap -mx-3 mb-2">
-  
-    <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Alamat Kantor
-      </label>
-      <textarea name = "a_alamatKantor" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-  
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Agama
-      </label>
-      <input name = "a_agama" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-       Gereja
-      </label>
-      <input name = "a_gereja" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-  
-  
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Tempat Lahir
-      </label>
-      <input name = "a_tempatLahir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-       Tanggal Lahir
-      </label>
-      <input name = "a_tglLahir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="date" />
-     
-    </div>
-  
-  
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-  
-  <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-      Alamat Rumah
-    </label>
-    <textarea name = "a_alamatRumah" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-   
-  </div>
-
-  </div>
-  <div className="flex flex-wrap -mx-3 mb-2">
-  
-  <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-      Nomor Handphone
-    </label>
-    <input name = "a_noHandphone" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-   
-  </div>
-
-  </div>
-
-  <h3 className='block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2 mt-7'>Identitas Ibu / Wali</h3>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Nama Ibu Kandung / Wali
-      </label>
-      <input name = "i_namaIbu" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />  
-    </div>
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Pendidikan Terakhir 
-      </label>
-      <input name = "i_pendidikanTerakhir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Pekerjaan
-      </label>
-      <input name = "i_pekerjaan" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />   
-    </div>
-    </div>
-
-    <div className="flex flex-wrap -mx-3 mb-2">
-  
-    <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Alamat Kantor
-      </label>
-      <textarea name = "i_alamatKantor" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-  
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Agama
-      </label>
-      <input name = "i_agama" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-       Gereja
-      </label>
-      <input name = "i_gereja" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-  
-  
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Tempat Lahir
-      </label>
-      <input name = "i_tempatLahir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-     
-    </div>
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-       Tanggal Lahir
-      </label>
-      <input name = "i_tglLahir" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="date" />
-     
-    </div>
-  
-  
-    </div>
-    <div className="flex flex-wrap -mx-3 mb-2">
-  
-  <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-      Alamat Rumah
-    </label>
-    <textarea name = "i_alamatRumah" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-   
-  </div>
-
-  </div>
-  <div className="flex flex-wrap -mx-3 mb-2">
-  
-  <div className="w-full md:w-1/1 px-3 mb-6 md:mb-0">
-    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-      Nomor Handphone
-    </label>
-    <input name = "i_noHandphone" onChange={handleOnChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" />
-   
-  </div>
-
-  </div>
 
   <button onClick={postData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
   Register
--- src/page/Tk.js
+++ src/page/Tk.js
@@ -67,14 +67,14 @@ export default function Tk() {
                 </div>
                 </div>
                 <div class="p-4 flex flex-col text-center items-center">
-                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
+                        <img className='h-44 w-44 rounded-full' src='/img/hendrawan.png' alt=''></img>
                         <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Hendrawan</h4>
                         <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Kepala Yayasan</p>
                     </div>
 
                 <div class="p-4 flex flex-col text-center items-center">
-                        <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0"></div>
-                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Erly</h4>
+                        <img className='h-44 w-44 rounded-full' src='/img/erly.png' alt=''></img>
+                        <h4 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Yap Erly</h4>
                         <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Sekretaris Yayasan</p>
                     </div>
                 <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
@@ -84,25 +84,120 @@ export default function Tk() {
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0">
                           <img className='h-44 w-44 rounded-full' src='/img/gurutk1.png' alt=''></img>
                         </div>
-                        <h4 class="sm:text-xl text-2xl font-medium title-font text-gray-900">Nama</h4>
-                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font text-gray-900">Yuliana Elizabeth Ginuni S. Pd</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Guru TK B</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0">
                         <img className='h-44 w-44 rounded-full' src='/img/busondang.png' alt=''></img>
                         </div>
                         <h4 class="sm:text-xl text-2xl font-medium title-font text-gray-900">I. Sondang Aviyanti P. S.Si</h4>
-                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Tata Usaha</p>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Kepala Sekolah</p>
                     </div>
                     <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                         <div className="bg-indigo-100 h-44 w-44 rounded-full  inline-flex items-center justify-center flex-shrink-0">
                         <img className='h-44 w-44 rounded-full' src='/img/gurutk2.png' alt=''></img>
                         </div>
-                        <h4 class="sm:text-xl text-2xl font-medium title-font text-gray-900">Nama</h4>
-                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Jabatan</p>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font text-gray-900">Anastasia A. Winarni S. Pd</h4>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Guru TK A</p>
                     </div>
                   
                     
+                
+                
+                </div>
+                
+            </div>
+            </section>
+            <section class="bg-gray-900 text-white body-font mt-5">
+            <div class="container px-5 mx-auto py-5">
+                <div class="text-center mb-10">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-white mb-4 ">Ekstrakulikuler</h1>
+                {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Berikut Struktur Organisasi dari TK Cerdas Bangsa</p> */}
+                <div class="flex mt-6 justify-center">
+                    <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+                </div>
+                </div>
+               
+
+              
+                <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+               
+                  
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44  w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                          <img className='h-44  w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-white">Ekstrakulikuler 1</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Guru TK B</p> */}
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44  w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                        <img className='h-44  w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-white">Ekstrakulikuler 2</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Kepala Sekolah</p> */}
+                    </div>
+                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44  w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                        <img className='h-44  w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-white">Ekstrakulikuler 3</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Guru TK A</p> */}
+                    </div>
+                  
+                    
+                
+                
+                </div>
+                
+            </div>
+            </section>
+            <section class="bg-white text-gray-900 body-font mt-5">
+            <div class="container px-5 mx-auto py-5">
+                <div class="text-center mb-10">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4 ">Fasilitas</h1>
+                {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Berikut Struktur Organisasi dari TK Cerdas Bangsa</p> */}
+                <div class="flex mt-6 justify-center">
+                    <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+                </div>
+                </div>
+               
+
+              
+                <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+               
+                  
+                    <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                          <img className='h-44 w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-gray-900">Fasilitas 1</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Guru TK B</p> */}
+                    </div>
+                    <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                        <img className='h-44 w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-gray-900">Fasilitas 2</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Kepala Sekolah</p> */}
+                    </div>
+                  
+                  
+                    <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                          <img className='h-44 w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-gray-900">Fasilitas 3</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Guru TK B</p> */}
+                    </div>
+                    <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                        <div className="bg-indigo-100 h-44 w-72 rounded-md  inline-flex items-center justify-center flex-shrink-0">
+                        <img className='h-44 w-72 rounded-md' src='' alt=''></img>
+                        </div>
+                        <h4 class="sm:text-xl text-2xl font-medium title-font mt-2 text-gray-900">Fasilitas 4</h4>
+                        {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Kepala Sekolah</p> */}
+                    </div>
                 
                 
                 </div>

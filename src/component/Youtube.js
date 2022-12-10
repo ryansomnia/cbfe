e34f33f55dc2e4f8d92a4832ae2292596f027b1f
Nightfall <marcelantonio246@gmail.com>
2022-12-10 14:57:05 +0700

    fix youtube

--- src/component/Youtube.js
+++ src/component/Youtube.js
@@ -2,19 +2,25 @@ import React from 'react'
 
 export default function Youtube() {
   return (
-    <div>
-             <section className='bg-green overflow-hidden mt-5'>
-      <div id="sejarah" className=" mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
-        <div className="w-full lg:w-6/12 pb-12">
-                  <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
-              </div>
-              <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-white pb-4">Youtube</h1>
-                  <p className="font-normal text-base leading-6 text-white ">Naikin nanti ke atas ya dikit cel </p>
-              </div>
-             
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="grid gap-12 row-gap-8 lg:grid-cols-2">
+        <div class="flex flex-col justify-center">
+          <div class="max-w-xl mb-6">
+            <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              The quick, brown fox<br class="hidden md:block" />
+              jumps over
+              <span class="inline-block text-deep-purple-accent-400">a lazy dog</span>
+            </h2>
+            <p class="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
+            </p>
           </div>
-          </section>
+          
+        </div>
+        <div>
+          <iframe class="object-cover w-full h-56 rounded shadow-lg sm:h-96" src="https://www.youtube.com/embed/_qXEDnBBCRs" title="Seventeen - Cinta Jangan Sembunyi (Official Lyric)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
     </div>
   )
 }

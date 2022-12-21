c2a383ae10c00bb96ebd8c34739e28c3359977a2
unknown <you@example.com>
2022-12-21 16:41:57 +0700

    galery scroll x

--- src/component/News.js
+++ src/component/News.js
@@ -1,58 +1,71 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from "axios";
 import moment from "moment"
-import {MdDateRange } from "react-icons/md";
+import { MdDateRange, MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 // import DetailArtikel from '../page/DetailArtikel';
-  export default function News() {
+export default function News() {
 
-    const [dataArtikel, setDataArtikel] = useState([]);
+  const [dataArtikel, setDataArtikel] = useState([]);
 
-    const navigate = useNavigate(); 
-    
-    useEffect(() => {
-    
-      const api = `http://8.215.37.21:3014/artikel/getNews`;
-  
-      const getData = async (e) => {
-          try {
-            let res = await axios.get(api);
-            setDataArtikel(res.data);
-            console.log(res.data);
-          } catch (err) {
-            console.log("err", err.response.status);
-          }
-        };
-  
-      getData();
-    }, []);
-    return (
-      <section className="pt-3 pb-12  overflow-hidden mt-5 ml-12">
-       <div className="flex justify-left">
-       {dataArtikel.map((x,i)=>
-  <div className="rounded-lg shadow-lg mr-7 bg-green max-w-sm">
-      <img className="rounded-t-lg" src={x.url} alt=""/>
-    <div className="p-6">
-      <h5 className="text-white text-xl font-medium mb-2">{x.judul}</h5>
-      <div className='flex flex-row'>
-        <MdDateRange size={25} color={'EDF6F3'}/>
-      <p className="text-white text-base mb-4">{moment(x.tglCreate).format("DD-MM-YYYY")}</p>
-      </div>
-      
-      <button type="button" 
-      className=" inline-block px-6 py-2.5 text-green font-medium text-xs bg-white
-       leading-tight uppercase rounded shadow-md hover:bg-green hover:shadow-lg 
-       focus:bg-green focus:shadow-lg focus:outline-none focus:ring-0
-        active:bg-green active:shadow-lg transition duration-150 ease-in-out" 
-        onClick={()=>navigate('detailArtikel', {state:{id:x.idartikel}})}
-        >Baca Selengkapnya</button>
-    </div>
-  </div>
-  )}
-</div>
+  const navigate = useNavigate();
 
-      </section>
-     
-    )
+  useEffect(() => {
+
+    const api = `http://8.215.37.21:3014/artikel/getNews`;
+
+    const getData = async (e) => {
+      try {
+        let res = await axios.get(api);
+        setDataArtikel(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log("err", err.response.status);
+      }
+    };
+
+    getData();
+  }, []);
+
+
+  const slideLeft = () => {
+    let slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft -  500
+  }
+
+  const slideRight = () => {
+    let slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft +  500
   }
   
+  return (
+
+    <div className='flex relative items-center m-4'>
+      <MdChevronLeft className='cursor-pointer' size={50} onClick={slideLeft}/>
+      <div id='slider' className="h-full w-full overflow-x-hidden scroll whitespace-nowrap scroll-smooth">
+        {dataArtikel.map((x, i) =>
+          <div className="rounded-lg shadow-lg mr-7 bg-green max-w-sm cursor-pointer inline-block">
+            <img className="rounded-t-lg hover:scale-105 ease-in-out duration-300 rounded-md" src={x.url} alt="" />
+            <div className="p-6">
+              <h5 className="text-white text-xl font-medium mb-2">{x.judul}</h5>
+              <div className='flex flex-row'>
+                <MdDateRange size={25} color={'EDF6F3'} />
+                <p className="text-white text-base mb-4">{moment(x.tglCreate).format("DD-MM-YYYY")}</p>
+              </div>
+
+              <button type="button"
+                className=" inline-block px-6 py-2.5 text-green font-medium text-xs bg-white
+       leading-tight uppercase rounded shadow-md hover:bg-green hover:shadow-lg 
+       focus:bg-green focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-green active:shadow-lg transition duration-150 ease-in-out"
+                onClick={() => navigate('detailArtikel', { state: { id: x.idartikel } })}
+              >Baca Selengkapnya</button>
+            </div>
+          </div>
+        )}
+      </div>
+      <MdChevronRight className='cursor-pointer' size={50} onClick={slideRight}/>
+    </div>
+
+  )
+}
--- src/page/Home.js
+++ src/page/Home.js
@@ -11,55 +11,55 @@ const Home = () => {
 
   return (
     <div>
-     <Napbar/>
-     <CarouselNews/>
-     <div className="flex flex-col">
-  
-      <section className='bg-green overflow-hidden mt-5'>
-      <div id="sejarah" className=" mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
-        <div className="w-full lg:w-6/12 pb-12">
-                  <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
-              </div>
-              <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-white pb-4">Selamat Datang </h1>
-                  <p className="font-normal text-base leading-6 text-white ">Kami sangat menyambut para visitor yang sudah mau berkunjung ke website kami.
-         Pertama tama kami mau memperkenalkan Sekolah Kristen Cerdas Bangsa yang berdomisili di Bogor dengan TK yang TERAKREDITASI B dan SD yang TERAKREDITASI A.
-          </p>
-              </div>
-             
-      </div>
-      </section>
-        <News/>
-        <div id="sejarah" className="mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
-            <div className="w-full lg:w-6/12 ">
-                  <img className="w-full h-full" src="/img/TK.JPG" alt="A group of People" />
+      <Napbar />
+      <CarouselNews />
+      <div className="flex flex-col">
+
+        <section className='bg-green overflow-hidden mt-5'>
+          <div id="sejarah" className=" mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
+            <div className="w-full lg:w-6/12 pb-12">
+              <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
             </div>
             <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">TK CERDAS BANGSA</h1>
-                <p className="font-normal text-base leading-6 text-gray-600 ">TK Cerdas Bangsa adalah pendidikan yang berbasis anak usia dini dengan dasar agama Kristen yang bertujuan untuk mendidik karakter anak - anak sejak usia dini, dengan fokus pengajaran kepada pengembangan kognitif, sosial emosional, fisik motorik dan materi pengetahuan lain yang disesuaikan dengan usia dini dan dikembangkan oleh guru - guru TK Cerdas Bangsa yang telah berpengalaman di bidangnya.</p>
+              <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-white pb-4">Selamat Datang </h1>
+              <p className="font-normal text-base leading-6 text-white ">Kami sangat menyambut para visitor yang sudah mau berkunjung ke website kami.
+                Pertama tama kami mau memperkenalkan Sekolah Kristen Cerdas Bangsa yang berdomisili di Bogor dengan TK yang TERAKREDITASI B dan SD yang TERAKREDITASI A.
+              </p>
             </div>
-             
+
+          </div>
+        </section>
+        <News />
+        <div id="sejarah" className="mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
+          <div className="w-full lg:w-6/12 ">
+            <img className="w-full h-full" src="/img/TK.JPG" alt="A group of People" />
+          </div>
+          <div className="w-full lg:w-5/12 flex flex-col justify-center">
+            <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">TK CERDAS BANGSA</h1>
+            <p className="font-normal text-base leading-6 text-gray-600 ">TK Cerdas Bangsa adalah pendidikan yang berbasis anak usia dini dengan dasar agama Kristen yang bertujuan untuk mendidik karakter anak - anak sejak usia dini, dengan fokus pengajaran kepada pengembangan kognitif, sosial emosional, fisik motorik dan materi pengetahuan lain yang disesuaikan dengan usia dini dan dikembangkan oleh guru - guru TK Cerdas Bangsa yang telah berpengalaman di bidangnya.</p>
+          </div>
+
         </div>
         <div id="sejarah" className="mx-12 flex flex-col lg:flex-row justify-between gap-8 ">
-        <div className="w-full lg:w-5/12 flex flex-col justify-center">
+          <div className="w-full lg:w-5/12 flex flex-col justify-center">
             <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">SD CERDAS BANGSA</h1>
-            <p className="font-normal text-base leading-6 text-gray-600 "> SD Cerdas bangsa, 
-SD Cerdas Bangsa didirikan Pada tanggal 01 Juli 2007, berdasarkan izin diknas Kabupaten Bogor Nomor : 421.2/243-Disdik/2010. Dengan Akreditasi A. Dan didirikan oleh Bapak Hendrawan. Tujuan didirikannya SD Cerdas Bangsa ini adalah untuk ikut berperan aktif mencerdaskan kehidupan bangsa dan negara khususnya di bidang pendidikan. Serta memajukan Desa cimandala dengan sarana pendidikan Sekolah Dasar. 
-</p>
+            <p className="font-normal text-base leading-6 text-gray-600 "> SD Cerdas bangsa,
+              SD Cerdas Bangsa didirikan Pada tanggal 01 Juli 2007, berdasarkan izin diknas Kabupaten Bogor Nomor : 421.2/243-Disdik/2010. Dengan Akreditasi A. Dan didirikan oleh Bapak Hendrawan. Tujuan didirikannya SD Cerdas Bangsa ini adalah untuk ikut berperan aktif mencerdaskan kehidupan bangsa dan negara khususnya di bidang pendidikan. Serta memajukan Desa cimandala dengan sarana pendidikan Sekolah Dasar.
+            </p>
+          </div>
+          <div className="w-full lg:w-6/12 ">
+            <img className="w-full h-full" src="/img/SD.JPG" alt="A group of People" />
+          </div>
         </div>
-        <div className="w-full lg:w-6/12 ">
-              <img className="w-full h-full" src="/img/SD.JPG" alt="A group of People" />
-        </div>
-        </div>
-          {/* <Youtube/> */}
-          
+        {/* <Youtube/> */}
 
 
 
 
-      
+
+
       </div>
-     <Footer/>
+      <Footer />
     </div>
   )
 }

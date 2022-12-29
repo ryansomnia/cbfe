6351b93e7b127da343c5c698192c464be94051f3
unknown <you@example.com>
2022-12-29 20:43:11 +0700

    responsive

--- src/page/DetailArtikel.js
+++ src/page/DetailArtikel.js
@@ -1,8 +1,11 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from "moment";
+import style from './style.module.css';
 import axios from "axios";
 import Napbar from './../component/Napbar'
 import Footer from '../component/Footer'
-import {useLocation} from 'react-router-dom';
+
+import { useLocation } from 'react-router-dom';
 export default function DetailArtikel() {
 
   const [dataArtikel, setDataArtikel] = useState([]);
@@ -11,44 +14,41 @@ export default function DetailArtikel() {
   useEffect(() => {
     const api = `http://89.116.228.164:3014/artikel/getOneNews`;
     const getData = async () => {
-        try {
-          let id = location.state.id;
-          let res = await axios.post(api, {id});
-       
-          setDataArtikel(res.data);
-        } catch (err) {
-         
-        }
-      };
+      try {
+        let id = location.state.id;
+        let res = await axios.post(api, { id });
+
+        setDataArtikel(res.data);
+      } catch (err) {
+
+      }
+    };
 
     getData();
   }, []);
   return (
-    
-    
+
+
     <div>
-        <Napbar/>
-      
-        <section className="pt-3 pb-36  overflow-hidden mt-5">
+      <Napbar />
+      <section className="pt-3 pb-36  overflow-hidden mt-5">
         {dataArtikel.map((artikel) =>
-        <div className="container px-4 py-2 mx-auto">
-     
-          <h2 className="mb-4 text-6xl md:text-5xl text-center font-bold font-heading font-heading tracking-px-n leading-tight text-gray-800">{artikel.judul}</h2>
-          <p className=" font-medium text-gray-800 text-center leading-relaxed md:max-w-lg mx-auto">{artikel.tglCreate}</p>
-          <div className=' w-auto h-96 mx-auto' >
-            {/* <h1 className='text-white'>{artikel.url}</h1> */}
-            <img id='news'
-        src={artikel.url}
-        className=" mx-auto"
-        alt={artikel.judul}/>
+          <div className="container px-4 py-2 mx-auto">
+            <h2 className="mb-4 text-6xl md:text-5xl text-center font-bold font-heading font-heading tracking-px-n leading-tight text-gray-800">{artikel.judul}</h2>
+            <p className=" font-medium text-gray-800 text-center leading-relaxed md:max-w-lg mx-auto">{moment(artikel.tglCreate).format("MMMM Do YYYY, h:mm:ss a")}</p>
+            <div className={style.bodyImg} >
+              <img id='news'
+                src={artikel.url}
+                className={style.imgCover}
+                alt={artikel.judul} />
+            </div>
+            <div className={style.bodyText} >
+              <p className={style.titleContent}>{artikel.isi}</p>
+            </div>
           </div>
-          <div className=' w-auto h-96 mx-auto mt-6' >
-            <h1 className='text-black'>{artikel.isi}</h1>
-          </div>
-      </div>
         )}
       </section>
-        <Footer/>
-        </div>
+      <Footer />
+    </div>
   )
 }
